e146e0526a66206f8da97bc77b3d2ed9d11c948d
Nick W <nicwws@gmail.com>
2022-01-25 14:10:48 +0800

    Functional add to database

--- covid_exposure.py
+++ covid_exposure.py
@@ -1,52 +1,43 @@
+#!/usr/bin/env python3
+
+# Import packages
 import os
+import sys
 import requests
 from bs4 import BeautifulSoup as bs
 import sqlite3
 
-##### START Config #####
+# Config values
 db_path = 'covid_expo.db'
 webpage = 'https://www.healthywa.wa.gov.au/Articles/A_E/Coronavirus/Locations-visited-by-confirmed-cases'
 
-##### END Config #####
 
-##### START Request and Process Data #####
-
-
-def request_covid_loc(page):
+def request_covid_loc(page):  # Request locations from webpage
 
     req = requests.get(page)
     soup = bs(req.content, 'lxml')
-    loc_table = soup.find(class_='locationList')
-    loc_rows = loc_table.find_all("div", {'id': 'mobileLocationList'})
+    table = soup.find('div', attrs={'id': 'locations'})
+    table_body = table.find_all('div', attrs={'id': 'mobileLocationList'})
 
-    return loc_rows
+    return table_body
 
 
-def parse_covid_loc(loc_html):
+def parse_covid_loc(table_body):  # Parse locations into text
 
     data = []
 
-    for _ in loc_html:
-        cols = _.find_all('p')
+    for row in table_body:
+        cols = row.find_all('p')
         cols = [ele.text.strip() for ele in cols]
         cols = [ele.replace('\n', ' ') for ele in cols]
         cols = [ele.replace(u'\xa0', ' ') for ele in cols]
+
         data.append([ele for ele in cols if ele])
 
     return data
 
 
-def print_sites(data):
-
-    for _ in data:
-        print(_)
-
-##### END Request and Process Data #####
-
-##### START Database Functions #####
 # Create database connection (If table doesnt exist, creates table)
-
-
 def create_connection(db_file):
 
     conn = None
@@ -56,7 +47,6 @@ def create_connection(db_file):
     except Exception as e:
         print(f"something went wrong: {e}")
 
-    # create tables if needed
     query = (
         "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='exposures';"
     )
@@ -80,23 +70,28 @@ def create_connection(db_file):
 
     return conn
 
-# Compare database and new list
 
+def filter_db_expo(dbconn, data):  # Compare database and new list
 
-def filter_db_expo(dbconn, data):
     new_alerts = []
     alerts_not_added = []
 
     for i in data:
-
-        if len(i) != 5:
+        if len(i) < 5:
             alerts_not_added.append(i)
+            print("lenght of list is wrong")
+            print(i)
         else:
-            timedate = i[0]
-            suburb = i[1]
-            location = i[2]
-            updated = i[3]
-            advice = i[4]
+            if len(i) > 5:
+                timedate = '\n'.join(i[:-4])
+            else:
+                timedate = i[0]
+            suburb = i[-4]
+            location = i[-3]
+            updated = i[-2]
+            advice = i[-1]
+
+            args = (timedate, suburb, location, updated, advice)
 
             query = """SELECT count(id) FROM exposures WHERE 
                     timedate = ? 
@@ -105,20 +100,18 @@ def filter_db_expo(dbconn, data):
                     AND updated = ?
                     AND advice = ?;"""
 
-            args = (timedate, suburb, location, updated, advice)
             result = dbconn.execute(query, args)
 
             if result.fetchone()[0] > 0:
                 pass
             else:
-                new_alerts.append(i)
+                new_alerts.append(args)
 
     return new_alerts, alerts_not_added
 
-# Add new sites to data base
 
+def add_site(dbconn, new_list):  # Add new sites to data base
 
-def add_site(dbconn, new_list):
     for i in new_list:
         timedate = i[0]
         suburb = i[1]
@@ -132,28 +125,47 @@ def add_site(dbconn, new_list):
 
         dbconn.execute(query, args)
 
-##### END Database Functions #####
+
+def print_sites(dbconn):  # Print all exposure sites
+    query = """SELECT * FROM exposures;"""
+    result = dbconn.execute(query)
+
+    for i in result:
+        print(i)
 
 
-def main():
-    # Create connection
-    dbconn = create_connection(db_path)
-
+def check_sites(dbconn):  # Function to check and add exposure sites to db
     # Request exposure sites
     sites_html = request_covid_loc(webpage)
     parsed_data = parse_covid_loc(sites_html)
-    # print_sites(parsed_data)
 
     # Check and insert exposure sites to database
     new_sites, not_added = filter_db_expo(dbconn, parsed_data)
-    # print(new_sites)
     add_site(dbconn, new_sites)
-    print_sites(not_added)
 
-    # # Close connection
+
+def main():
+    user_input = input("Please select from 'add', 'print' or 'exit': ")
+
+    if user_input == 'exit':
+        print('Exit program')
+        sys.exit()
+
+    # Create connection
+    dbconn = create_connection(db_path)
+
+    if user_input == 'add':
+        check_sites(dbconn)
+
+    elif user_input == 'print':
+        print_sites(dbconn)
+
+    else:
+        print("Wrong input, program terminated")
+
+    # Close connection
     dbconn.commit()
     dbconn.close()
-    # print("Program ran to completion")
 
 
 if __name__ == '__main__':
